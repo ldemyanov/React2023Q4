6609d2c5f65f8a03e50f7460df35663643f669f7
ldemyanov <leonidemyanov@gmail.com>
2023-10-25 19:05:22 +0800

    fix: add tailwind and vite-plugin-svrg

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./index.css";
 import SearchPage from './SearchPage.tsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
