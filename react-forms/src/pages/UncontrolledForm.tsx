1d2fa76371a338e365b1e48686e40cbb56e0841f
ldemyanov <leonidemyanov@gmail.com>
2023-12-01 21:35:24 +0800

    feat: add layout uncontrolled form

--- react-forms/src/pages/UncontrolledForm.tsx
+++ react-forms/src/pages/UncontrolledForm.tsx
@@ -1,5 +1,32 @@
 const UncontrolledForm: React.FC = () => {
-  return <>UncontrolledForm</>;
+  return (
+    <form>
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input type="text" name="name" placeholder="Leonid" />
+      </div>
+
+      <div>
+        <label htmlFor="age">Age: </label>
+        <input type="number" name="age" placeholder="23" />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email: </label>
+        <input type="text" name="email" placeholder="de17eon@gmail.com" />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="password" name="password" />
+      </div>
+
+      <div>
+        <label htmlFor="password2">Repeat password: </label>
+        <input type="password" name="password2" />
+      </div>
+    </form>
+  );
 };
 
 export default UncontrolledForm;
